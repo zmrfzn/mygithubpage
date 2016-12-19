07562eebabe2ac5497f6426bf4d991d187f0660b
zameer <zmrfzn@gmail.com>
2016-12-19 23:26:22 +0530

    updated loaderjs progress

--- MySite2.0/js/custom/loader.js
+++ MySite2.0/js/custom/loader.js
@@ -1,10 +1,12 @@
 $(window).load(function() {
-    setTimeout(function() {
+ //   setTimeout(function() {
+ 	/*NProgress.inc();*/
     NProgress.done();
-    }, 3000);
+   // }, 3000);
 });
 
 $(document).ready(function() {
     NProgress.start();
-
+    NProgress.set(0.1);
+    /*NProgress.inc();*/
 });
